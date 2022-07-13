b278cc20e964e6830e94b623718d2509d6b7bc3d
Gordon Moore <gmgithub@gordonmoore.co.uk>
2022-07-13 09:39:27 +0100

    Added basic mapsui Quickstart

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,23 +2,14 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
 	public MainPage()
 	{
 		InitializeComponent();
-	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    var mapControl = new Mapsui.UI.Maui.MapControl();
+    mapControl.Map?.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
+    Content = mapControl;
+  }
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
 }
 
--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace MapsuiTest;
+﻿using SkiaSharp.Views.Maui.Controls.Hosting;
+
+namespace MapsuiTest;
 
 public static class MauiProgram
 {
@@ -7,12 +9,16 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
+      .UseSkiaSharp()
+      .ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
 
-		return builder.Build();
+    //dependency injection
+    //builder.Services.AddTransient<MainPage>();
+    //builder.Services.AddTransient<MainPageViewModel>();
+    return builder.Build();
 	}
 }
